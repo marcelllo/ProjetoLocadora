c24de7122771413b12e344dc698b68132de1020d
Marcelo Santos <marcello.santos@gmail.com>
2020-03-21 20:13:34 -0300

    Aula 10 - Classe CategoriaDAO - Método listar categoria - Parte 1

--- src/persistencia/CategoriaDAO.java
+++ src/persistencia/CategoriaDAO.java
@@ -3,6 +3,9 @@ package persistencia;
 import entidades.Categoria;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class CategoriaDAO {
     public static boolean inserir(Categoria categoria) {
@@ -56,5 +59,27 @@ public class CategoriaDAO {
             System.out.println(e.getMessage());
             return false;
         }
+    }
+        
+    public static ArrayList<Categoria> listar() {
+        ArrayList<Categoria> categorias = new ArrayList<Categoria>();
+        try {
+            Connection conexao = Conexao.getConexao();
+            String sql = "SELECT * FROM categoria";
+            Statement comando = conexao.createStatement();
+            ResultSet resultado = comando.executeQuery(sql);
+            while(resultado.next()) {
+                Categoria c = new Categoria();
+                c.setId(resultado.getInt("id"));
+                c.setNome(resultado.getString("nome"));
+                c.setTipo(resultado.getString("tipo").charAt(0));
+                categorias.add(c);
+            }
+            resultado.close();
+            comando.close();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return categorias;
     }
 }
