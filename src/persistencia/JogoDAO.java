a6f1881594850176018d9aaa0d84e3b045114419
Marcelo Santos <marcello.santos@gmail.com>
2020-05-11 21:13:27 -0300

    Aula 16: JogoDAO - Métodos alterar e excluir

--- src/persistencia/JogoDAO.java
+++ src/persistencia/JogoDAO.java
@@ -31,4 +31,44 @@ public class JogoDAO {
             return false;
         }
     }
+    
+    public static boolean alterar(Jogo jogo) {
+        try {
+            Connection conexao = Conexao.getConexao();
+            String sql = "UPDATE jogo SET "
+                    + "titulo=?, descricao=?, preco=?, numeroDias=?, "
+                    + "categoria=?, memoria=?, tipo=? "
+                    + "WHERE id=?";
+            PreparedStatement ps = conexao.prepareStatement(sql);
+            ps.setString(1, jogo.getTitulo());
+            ps.setString(2, jogo.getDescricao());
+            ps.setDouble(3, jogo.getPreco());
+            ps.setInt(4, jogo.getNumeroDias());
+            ps.setString(5, jogo.getCategoria().getNome());
+            ps.setInt(6, jogo.getMemoria());
+            ps.setString(7, String.valueOf(jogo.getTipo()));
+            ps.setInt(8, jogo.getId());
+            int resultado = ps.executeUpdate();
+            ps.close();
+            return resultado > 0;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
+    
+    public static boolean excluir(int id) {
+        try {
+            Connection conexao = Conexao.getConexao();
+            String sql = "DELETE FROM jogo WHERE id=?";
+            PreparedStatement ps = conexao.prepareStatement(sql);
+            ps.setInt(1, id);
+            int resultado = ps.executeUpdate();
+            ps.close();
+            return resultado > 0;
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+        }
+    }
 }
